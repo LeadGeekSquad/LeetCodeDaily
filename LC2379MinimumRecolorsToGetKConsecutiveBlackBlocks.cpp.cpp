8638d9e7fdc84140454847aaad16e6ba84bb0833
LeadGeekSquad <namanpatel1997@yahoo.com>
2025-03-10 00:20:41 -0500

    LC2379 MinimumRecolorsToGetKConsecutiveBlackBlocks

--- LC2379MinimumRecolorsToGetKConsecutiveBlackBlocks.cpp.cpp
+++ LC2379MinimumRecolorsToGetKConsecutiveBlackBlocks.cpp.cpp
@@ -11,7 +11,16 @@ class Solution {
 
      /**
       * Approach:
-      * - 
+      * - we will be using the sliding window technique to find the K consecutive black blocks
+      * - we will have two pointers, to kind of create a window of size K
+      * - we will have a whiteCount variable to keep track of how many recolors we will need
+      * - in the worst case as our result we might have K recolors, so we create a variable result and assign it with K
+      * - we keep iterating to the right until our window size while checking for W and increasing the white count
+      * - to check if our i reaches the size of window K, we do i - left + 1
+      * - and because we have reached the size of window, we will update the result with min values of (result,whiteCount)
+      * - before we increment our left, we make sure that if the char at left is 'W' we remove from the whiteCount, as our window size is now being changed
+      * - and then increment our left
+      * - while we are in our window size limit, we check if our block[right] is 'W' we increment our whiteCount
       */
      
       public:
@@ -21,7 +30,7 @@ class Solution {
           int result = k;
           int whiteCount = 0;
           int left = 0;
-  
+          //treat i as the right pointer, that will be moving towards the end
           for(int i = 0 ; i < n; i++) {
   
               if(blocks[i] == 'W') {
@@ -43,7 +52,7 @@ class Solution {
       }
 
         /**
-         * Time Complexity:
-         * Space Complexity:
+         * Time Complexity: O(n)  atmost we might be visiting few element 2 times
+         * Space Complexity: O(1) no extra space utilized
          */
     };
